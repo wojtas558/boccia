199d7a7c5646830c7d7c65b43396b20a7e35ea83
wojtas558 <wojtek20052006@gmail.com>
2025-09-23 14:50:29 +0200

    Control page and timer prototype

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -2,11 +2,11 @@ export default function Ball({
   getColor,
   scored = false,
   height = 80,
-  onClick = {},
+  onClick,
 }) {
   return (
     <span
-      onClick={() => onClick()}
+      onClick={onClick}
       className={'border rounded-circle d-block ' + (scored ? getColor() : '')}
       style={{
         height: height,
