27aa8d54a01ed1fadcba059802bf0ab5e624cdec
Rainbow P <pablososek@gmail.com>
2025-09-23 19:24:23 +0200

    Change dashboard to me black themed also change button names and display button color

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,16 +1,16 @@
 export default function Match({ data }) {
   return (
-    <div className='border border-black border-2 my-2 rounded d-flex match text-center'>
+    <div className='border border-white border-opacity-25 border-2 my-2 rounded d-flex match text-center'>
       {/* <div className='border-end border-2 border-black py-auto'>ID: {data}</div> */}
       <div className='px-0'>
-        <div className='px-3 border-bottom border-2 border-black'>
+        <div className='px-3 border-bottom border-2 border-white border-opacity-25'>
           AAAAA BBBBB CCCCC vs AAAAA BBBBB CCCCC
         </div>
         <div className='px-3'>PULA B - RUNDA 1 - KORT 3 </div>
       </div>
-      <div className='ms-auto border-start border-2 border-black d-flex'>
-        <button className='btn btn-outline-primary me-3'>Display</button>
-        <button className='btn btn-outline-warning'>Control</button>
+      <div className='ms-auto border-start border-2 border-white border-opacity-25 d-flex'>
+        <button className='btn btn-outline-info me-3'>Wyświetlanie</button>
+        <button className='btn btn-outline-warning'>Zarządzanie</button>
       </div>
     </div>
   );
